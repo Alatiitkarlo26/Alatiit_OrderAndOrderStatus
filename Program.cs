915c7702076f0e45923b8e118689017d24c7e5ac
Andrea <concepciondeng25@gmail.com>
2026-02-21 10:45:46 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ALATIIIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Order and Order Status --");
+            Console.WriteLine("Do you want to Order? y/n");
+            
+            
+
+        }
+    }
+}
