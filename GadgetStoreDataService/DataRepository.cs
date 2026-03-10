31749f4d464df2605d7eed74b4a975996499e9b4
Alatiitkarlo26 <karloalatiit0@gmail.com>
2026-03-10 18:53:25 +0800

    update to the seperation of concerns
    improvised the whole console program file

---
+++ GadgetStoreDataService/DataRepository.cs
@@ -0,0 +1,24 @@
+﻿using GadgetStoreModels;
+
+namespace GadgetStoreDataService
+{
+   
+        public class DataRepository
+        {
+            public List<Product> GetProducts()
+            {
+                return new List<Product>
+            {
+            new Product { Name = "Laptop", Price = 800 },
+            new Product { Name = "Smartphone", Price = 500 },
+            new Product { Name = "Headphones", Price = 75 },
+            new Product { Name = "Keyboard", Price = 40 },
+            new Product { Name = "Mouse", Price = 25 },
+            new Product { Name = "PC", Price = 2000 },
+            new Product { Name = "Monitor", Price = 150 }
+            };
+            }
+
+            // We use a static list to "persist" data while the app is running
+            public static List<Transaction> History = new List<Transaction>();
+        } }
---
+++ GadgetStoreModels/StoreModels.cs
@@ -0,0 +1,16 @@
+﻿namespace GadgetStoreModels
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Summary { get; set; }
+        public double TotalPaid { get; set; }
+    }
+}
+
